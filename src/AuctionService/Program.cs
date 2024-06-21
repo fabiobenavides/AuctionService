3c248aa97b52296416d55f0831dea19c87bb7e57
Fabio Benavides <Fabio.Benavides@Henryscheinone.com>
2024-06-21 15:33:19 -0600

    Adding Postgres database (Docker)

--- src/AuctionService/Program.cs
+++ src/AuctionService/Program.cs
@@ -1,8 +1,16 @@
+using AuctionService.Data;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-
 builder.Services.AddControllers();
+builder.Services.AddDbContext<AuctionDbContext>(opt => 
+{
+    opt.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection"));
+});
 
 var app = builder.Build();
 
