c8bf524582c91b586cdbe303a206eec3e89c4158
Fabio Benavides <Fabio.Benavides@Henryscheinone.com>
2024-07-02 09:26:21 -0600

    Calling the service by httpClient

---
+++ src/SearchService/Services/AuctionServiceHttpClient.cs
@@ -0,0 +1,27 @@
+﻿using MongoDB.Entities;
+
+namespace SearchService;
+
+public class AuctionServiceHttpClient
+{
+    private readonly HttpClient _httpClient;
+    private readonly IConfiguration _configuration;
+
+    public AuctionServiceHttpClient(HttpClient client, IConfiguration configuration)
+    {
+        _httpClient = client;
+        _configuration = configuration;
+    }
+
+    public async Task<List<Item>> GetItemsForSearchDb()
+    {
+        var lastUpdated = await DB.Find<Item, string>()
+            .Sort(x => x.Descending(x => x.UpdatedAt))
+            .Project(x => x.UpdatedAt.ToString())
+            .ExecuteFirstAsync();
+
+        return await _httpClient
+            .GetFromJsonAsync<List<Item>>(
+                _configuration["AuctionServiceUrl"] + "/api/auctions?date=" + lastUpdated);
+    }
+}
