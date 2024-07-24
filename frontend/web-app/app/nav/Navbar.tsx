f1a0b13c5b29c7643c467f468cd8b7a4a5ffc6fc
Fabio Benavides <Fabio.Benavides@Henryscheinone.com>
2024-07-24 08:16:07 -0600

    Starting with the navbar

---
+++ frontend/web-app/app/nav/Navbar.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export default function Navbar() {
+  return (
+    <header className='sticky top-0 z-50 flex justify-between'>
+      <div>Left</div>
+      <div>Middle</div>
+      <div>Right</div>
+    </header>
+  )
+}
