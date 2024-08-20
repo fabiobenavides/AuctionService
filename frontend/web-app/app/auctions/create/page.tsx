c0318a934f084f005192a51e66d48ae55e553167
Fabio Benavides <Fabio.Benavides@Henryscheinone.com>
2024-08-20 08:14:58 -0600

    Adding react-hook-form

--- frontend/web-app/app/auctions/create/page.tsx
+++ frontend/web-app/app/auctions/create/page.tsx
@@ -1,7 +1,12 @@
+import Heading from '@/app/components/Heading'
 import React from 'react'
+import AutionForm from '../AuctionForm'
 
 export default function Create() {
   return (
-    <div>Create</div>
+    <div className='mx-auto max-w-[75%] shadow-lg bg-white rounded-lg'>
+      <Heading title='Sell your car' subtitle='Enter details' />
+      <AutionForm />
+    </div>
   )
 }
