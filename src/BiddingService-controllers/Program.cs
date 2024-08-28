1adfc60735fb1106bb5828cc6087b9cc72ee3cd4
Fabio Benavides <Fabio.Benavides@Henryscheinone.com>
2024-08-28 08:12:13 -0600

    Adding the new Biding service

---
+++ src/BiddingService-controllers/Program.cs
@@ -0,0 +1,42 @@
+using MassTransit;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using MongoDB.Driver;
+using MongoDB.Entities;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddMassTransit( x => 
+{
+    x.SetEndpointNameFormatter(new KebabCaseEndpointNameFormatter("bids", false));
+
+    x.UsingRabbitMq((context, cfg) => 
+    {
+        cfg.Host(builder.Configuration["RabbitMQ:Host"], "/", h => {
+            h.Username(builder.Configuration.GetValue("RabbitMQ:Username", "guest"));
+            h.Password(builder.Configuration.GetValue("RabbitMQ:Password", "guest"));
+        });
+        cfg.ConfigureEndpoints(context);
+    });
+});
+
+builder.Services
+    .AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+    .AddJwtBearer(options => 
+    {
+        options.Authority = builder.Configuration["IdentityServiceUrl"];
+        options.RequireHttpsMetadata = false; //we are not using https for now
+        options.TokenValidationParameters.ValidateAudience = false;
+        options.TokenValidationParameters.NameClaimType = "username";
+    });
+
+var app = builder.Build();
+
+
+app.UseHttpsRedirection();
+
+await DB.InitAsync("BidDb", MongoClientSettings
+    .FromConnectionString(builder.Configuration.GetConnectionString("BidDbConnection")));
+
+app.Run();
+
