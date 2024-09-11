8b1ebf6660b079186ec0b516a563981a4dcffaf8
Fabio Benavides <Fabio.Benavides@Henryscheinone.com>
2024-09-11 08:16:26 -0600

    Adding global store

--- frontend/web-app/app/auctions/Listings.tsx
+++ frontend/web-app/app/auctions/Listings.tsx
@@ -4,16 +4,17 @@ import React, { useEffect, useState } from 'react'
 import AuctionCard from './AuctionCard';
 import AppPagination from '../components/AppPagination';
 import { getData } from '../actions/auctionActions';
-import { Auction, PagedResult } from '@/types';
 import Filters from './Filters';
 import { useParamsStore } from '@/hooks/useParamsStore';
-import { shallow } from 'zustand/shallow';
 import queryString from 'query-string';
 import EmptyFilter from '../components/EmptyFilter';
+import { useAuctionStore } from '@/hooks/useAuctionStore';
+import { useShallow } from 'zustand/react/shallow';
 
 export default function Listings() {
-  const [data, setData] = useState<PagedResult<Auction>>();
-  const params = useParamsStore(state => ({
+  const [loading, setLoading] = useState(true);
+
+  const params = useParamsStore(useShallow(state => ({
     pageNumber: state.pageNumber,
     pageSize: state.pageSize,
     searchTerm: state.searchTerm,
@@ -21,7 +22,16 @@ export default function Listings() {
     filterBy: state.filterBy,
     seller: state.seller,
     winner: state.winner
-  }), shallow)
+  })))
+
+  const data = useAuctionStore(useShallow(state => ({
+    auctions: state.auctions,
+    totalCount: state.totalCount,
+    pageCount: state.pageCount
+  })))
+
+  const setData = useAuctionStore(state => state.setData);
+
   const setParams = useParamsStore(state => state.setParams)
   const url = queryString.stringifyUrl({url: '', query: params})
 
@@ -32,10 +42,11 @@ export default function Listings() {
   useEffect(() => {
     getData(url).then(data => {
       setData(data);
+      setLoading(false);
     })
   }, [url]);
 
-  if (!data) return <h3>Loading...</h3>
+  if (loading) return <h3>Loading...</h3>
 
   return (
     <>
@@ -46,7 +57,7 @@ export default function Listings() {
           : (
             <>
               <div className='grid grid-cols-4 gap-6'>
-                {data.results.map((auction) => (
+                {data.auctions.map((auction) => (
                   <AuctionCard auction={auction} key={auction.id} />
                 ))}
               </div>
