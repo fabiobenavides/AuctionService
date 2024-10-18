7550e1ea64b75e107c2acfce79d04bd35f165fd2
Fabio Benavides <Fabio.Benavides@Henryscheinone.com>
2024-10-18 08:26:50 -0600

    Adding integration tests for AuctionController

--- test/AuctionService.IntegrationTests/AuctionControllerTests.cs
+++ test/AuctionService.IntegrationTests/AuctionControllerTests.cs
@@ -1,7 +1,11 @@
 using System;
+using System.Net;
+using System.Net.Http.Json;
 using AuctionService.Data;
+using AuctionService.Dtos;
 using AuctionService.IntegrationTests.Fixtures;
 using AuctionService.IntegrationTests.Util;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.DependencyInjection;
 
 namespace AuctionService.IntegrationTests;
@@ -9,11 +13,54 @@ namespace AuctionService.IntegrationTests;
 public class AuctionControllerTests : IClassFixture<CustomWebAppFactory>, IAsyncLifetime
 {
     private readonly CustomWebAppFactory _factory;
+    private readonly HttpClient _httpClient;
+    private const string VEYRON_ID = "c8c3ec17-01bf-49db-82aa-1ef80b833a9f";
 
     public AuctionControllerTests(CustomWebAppFactory factory)
     {
         _factory = factory;
+        _httpClient = factory.CreateClient();
     }
+
+    [Fact]
+    public async Task GetAuctions_ShouldReturn3Auctions()
+    {
+        // Arrange ?
+
+        // Act
+        var result = await _httpClient.GetFromJsonAsync<List<AuctionDto>>("api/auctions");
+
+        // Assert
+        Assert.Equal(3, result.Count);
+    }
+
+    [Fact]
+    public async Task GetAuctionById_WithValidId_ShouldReturnAuction()
+    {
+        // Arrange ?
+
+        // Act
+        var result = await _httpClient.GetFromJsonAsync<AuctionDto>("api/auctions/" + VEYRON_ID);
+
+        // Assert
+        Assert.NotNull(result);
+        Assert.Equal("Bugatti", result.Make);
+        Assert.Equal("Veyron", result.Model);
+    }
+
+    [Fact]
+    public async Task GetAuctionById_WithInvalidId_ShouldReturnNotFound()
+    {
+        // Arrange ?
+
+        // Act
+        var result = await _httpClient.GetAsync("api/auctions/" + Guid.NewGuid());
+
+        // Assert
+        Assert.Equal(HttpStatusCode.NotFound, result.StatusCode);
+    }
+
+
     public Task InitializeAsync()
     {
         return Task.CompletedTask;
